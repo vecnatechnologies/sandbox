8df75a926510de5b29a18a6ff06ddcfaa30bc3ac
Oleg Golberg <ogolberg@vecna.com>
2015-06-24 14:34:35 -0400

    adding tests #2

--- test.js
+++ test.js
@@ -5,4 +5,8 @@ describe('#add', function() {
   it('should add numbers', function() {
     assert.equal(5, add(2, 3));
   });
+
+  it('should add numbers', function() {
+    assert.equal(7, add(4, 3));
+  });
 });
